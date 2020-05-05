be5d6440ffd8a70ba1791570ecb38db4f99af293
Lyon Liang <lyon.liang12@gmail.com>
2020-05-05 14:41:03 +0800

    finish 03

--- lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -2,15 +2,25 @@ import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
 import 'package:number_trivia/core/error/failures.dart';
+import 'package:number_trivia/core/usecases/usecase.dart';
 import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 
-class GetConcreteNumberTrivia extends Equatable {
+class GetConcreteNumberTrivia
+    implements UseCase<NumberTrivia, GetConcreteNumberTriviaParams> {
   final NumberTriviaRepository respository;
 
   GetConcreteNumberTrivia(this.respository);
 
-  Future<Either<Failure, NumberTrivia>> execute({@required int number}) async {
-    return await respository.getConcreteNumberTrivia(number);
+  @override
+  Future<Either<Failure, NumberTrivia>> call(
+      GetConcreteNumberTriviaParams params) async {
+    return await respository.getConcreteNumberTrivia(params.number);
   }
+}
+
+class GetConcreteNumberTriviaParams extends Equatable {
+  final int number;
+
+  GetConcreteNumberTriviaParams({@required this.number}) : super([number]);
 }
